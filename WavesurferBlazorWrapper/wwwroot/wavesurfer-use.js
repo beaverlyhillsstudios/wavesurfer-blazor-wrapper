1a37430aa2927af56f43bb7731cd1dddab40dd36
Roman Štefek <roman@stefkovi.info>
2022-02-23 09:18:39 +0100

    Added all methods, version bump

--- WavesurferBlazorWrapper/wwwroot/wavesurfer-use.js
+++ WavesurferBlazorWrapper/wwwroot/wavesurfer-use.js
@@ -32,7 +32,7 @@ export function create(dotNetHelper, mainDivGuid, timelineDivGuid, minimapDivGui
 
     wavesurfer = WaveSurfer.create(options);
     
-    //Events
+    //WaveSurfer events
     wavesurfer.on('audioprocess',
         function(position) {
             dotNetHelper.invokeMethodAsync("OnWavesurferAudioProcess", position);
@@ -110,22 +110,138 @@ export function create(dotNetHelper, mainDivGuid, timelineDivGuid, minimapDivGui
     );
 }
 
-export function load(url) {
-    wavesurfer.load(url);
+//Wavesurfer methods
+export function cancelAjax()
+    wavesurfer.cancelAjax();
 }
-
+export function destroy() {
+    wavesurfer.destroy();
+}
+export function empty() {
+    wavesurfer.empty();
+}
+export function getActivePlugins() {
+    return wavesurfer.getActivePlugins();
+}
+export function getBackgroundColor() {
+    return wavesurfer.getBackgroundColor();
+}
+export function getCurrentTime() {
+    return wavesurfer.getCurrentTime();
+}
+export function getCursorColor() {
+    return wavesurfer.getCursorColor();
+}
+export function getDuration() {
+    return wavesurfer.getDuration();
+}
+export function getPlaybackRate() {
+    return wavesurfer.getPlaybackRate();
+}
+export function getProgressColor() {
+    return wavesurfer.getProgressColor();
+}
+export function getVolume() {
+    return wavesurfer.getVolume();
+}
+export function getMute() {
+    return wavesurfer.getMute();
+}
+export function getFilters() {
+    return wavesurfer.getFilters();
+}
+export function getWaveColor() {
+    return wavesurfer.getWaveColor();
+}
+export function exportPCM(length, accuracy, noWindow, start) {
+    return wavesurfer.exportPCM(length, accuracy, noWindow, start);
+}
+export function exportImage(format, quality, type) {
+    return wavesurfer.exportImage(format, quality, type);
+}
+export function isPlaying() {
+    return wavesurfer.isPlaying();
+}
+export function load(url, peaks, preload) {
+    wavesurfer.load(url, peaks, preload);
+}
+export function loadBlob(url) {
+    wavesurfer.loadBlob(url);
+}
+export function pause() {
+    wavesurfer.pause();
+}
+export function play(start, end) {
+    wavesurfer.play(start, end);
+}
 export function playPause() {
     wavesurfer.playPause();
 }
-
-export function zoom(zoomLevel) {
-    wavesurfer.zoom(zoomLevel);
+export function seekAndCenter(progress) {
+    wavesurfer.seekAndCenter(progress);
+}
+export function seekTo(progress) {
+    wavesurfer.seekTo(progress);
+}
+export function setBackgroundColor(color) {
+    wavesurfer.setBackgroundColor(color);
+}
+export function setCursorColor(color) {
+    wavesurfer.setCursorColor(color);
+}
+export function setHeight(height) {
+    wavesurfer.setHeight(height);
+}
+export function setFilter(filters) {
+    wavesurfer.setFilter(filters);
+}
+export function setPlaybackRate(rate) {
+    wavesurfer.setPlaybackRate(rate);
+}
+export function setPlayEnd(position) {
+    wavesurfer.setPlayEnd(position);
+}
+export function setVolume(newVolume) {
+    wavesurfer.setVolume(newVolume);
+}
+export function setMute(mute) {
+    wavesurfer.setMute(mute);
+}
+export function setProgressColor(color) {
+    wavesurfer.setProgressColor(color);
+}
+export function setWaveColor(color) {
+    wavesurfer.setWaveColor(color);
+}
+export function skip(offset) {
+    wavesurfer.skip(offset);
+}
+export function skipBackward() {
+    wavesurfer.skipBackward();
+}
+export function skipForward() {
+    wavesurfer.skipForward();
+}
+export function setSinkId(color) {
+    wavesurfer.setSinkId(color);
+}
+export function stop() {
+    wavesurfer.stop();
+}
+export function toggleMute() {
+    wavesurfer.toggleMute();
+}
+export function toggleInteraction() {
+    wavesurfer.toggleInteraction();
+}
+export function toggleScroll() {
+    wavesurfer.toggleScroll();
+}
+export function zoom(pxPerSec) {
+    wavesurfer.zoom(pxPerSec);
 }
 
-export function seekAndCenter(seek) {
-    wavesurfer.seekAndCenter(seek);
-}
-
+//utility functions
 export function loadRegions(regions) {
     wavesurfer.clearRegions();
     for (var x = 0; x < regions.length; x++) {
